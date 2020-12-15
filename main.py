6fb9767e374327bfa5e6cf50f120391dbbbb2e4b
Subrhmanya-Devadiga <70996082+Subrhmanya-Devadiga@users.noreply.github.com>
2020-12-15 21:50:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,26 +10,28 @@ def loop():
     jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
     for job in jobs:
         published_date = job.find('span',class_='sim-posted').span.text
-        if 'few' in published_date:
+        if 'few' or '1' or '2' or '3' in published_date:
             company_name = job.find('h3', class_='joblist-comp-name').text
             skills_required = job.find('span', class_='srp-skills').text.replace(' ','')
             more_info = job.header.h2.a['href']
+            date = job.find('span', class_='sim-posted').span.text
             print(f"\t Company Name: {company_name.strip()}")
             print(f"\t Required skill: {skills_required.strip()}")
+            print(f"\t Date: {date.strip()}")
             print(f"\t More_info: {more_info}")
             print('\n')
 # saving the fetched data to the database using sqlite3
             con = sqlite3.connect('database.db')
             c = con.cursor()
             try:
-                c.execute("CREATE TABLE Job_Info(Company_Name text, Skills_Required text, More_info text)")  
+                c.execute("CREATE TABLE Job_Info(Company_Name text, Skills_Required text, More_info text)")
             except:
                 pass
             c.execute("INSERT INTO Job_Info VALUES(?,?,?)",(company_name,skills_required,more_info,))
             c.execute("SELECT rowid, * FROM Job_Info ")
-        con.commit()
+            con.commit()
     con.close()
-
+# deleting old entery's
 def deletedatabase():
     try:
         con = sqlite3.connect("database.db")
